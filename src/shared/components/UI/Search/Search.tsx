29df24ff11a8ca8737f38655cb4e0a0ded8d23c6
29453komandor@gmail.com <29453komandor@gmail.com>
2024-05-16 17:23:55 +0300

    added search

--- src/shared/components/UI/Search/Search.tsx
+++ src/shared/components/UI/Search/Search.tsx
@@ -1,28 +1,93 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import styles from "./Search.module.scss";
 import { useLocation } from "react-router-dom";
 import { IconSearch } from "../../../icons/SearchIcon";
 import useSearch from "../../../hooks/useSearch";
+import { useFindCharactersByNameQuery } from "../../../../api/redux/api/card-api";
+import { useDebounce } from "use-debounce";
+import { ClipLoader } from "react-spinners";
+import { NothingYet } from "../../NothingYet/NothingYet";
+import { CharecterCard } from "../../Card/Card";
 
 const isSearchBarVisible = (pathname: string): boolean => {
   if (pathname === "/signin" || pathname === "/signup") return false;
   return true;
 };
 
+interface SuggestItemProps {
+  image: string;
+  name: string;
+}
+
+const SuggestItem = ({ image, name }: SuggestItemProps) => {
+  return (
+    <div className={styles.itemWrapper}>
+      <div className={styles.img}>
+        <img src={image} alt="character" />
+      </div>
+      <span>{name}</span>
+    </div>
+  );
+};
+
+interface SuggestListProps {
+  data: CharecterCard[] | undefined;
+  value: string | null;
+  isFetching: boolean;
+  isSucces: boolean;
+  isLoading?: boolean;
+  isError?: boolean;
+}
+
+const SuggestList = ({
+  data,
+  value,
+  isFetching,
+  isSucces,
+  isError,
+  isLoading,
+}: SuggestListProps) => {
+  return (
+    <>
+      <div className={styles.suggestList}>
+        {isFetching || !value ? (
+          <div className={styles.loader}>
+            <ClipLoader color="#36d7b7" />
+          </div>
+        ) : !isSucces ? (
+          <div className={styles.notFound}>
+            <span>Not found</span>
+          </div>
+        ) : null}
+        {!isError && data
+          ? data.map((el, i) => <SuggestItem key={i} {...el} />)
+          : null}
+      </div>
+    </>
+  );
+};
+
 export const Search = () => {
   const { pathname } = useLocation();
   const { searchQuery, toggleOnChange, toggleSearch } = useSearch();
+  const [value] = useDebounce(searchQuery, 1000);
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     toggleSearch();
   };
 
+  const { data, isFetching, isSuccess, isLoading, isError } =
+    useFindCharactersByNameQuery(value ?? "", { skip: value ? false : true });
+
   return (
     <>
       {isSearchBarVisible(pathname) ? (
         <form onSubmit={handleSubmit} className={styles.searchForm} action="">
           <input
+            onFocus={() => setIsVisible(true)}
+            onBlur={() => setIsVisible(false)}
             type="text"
             placeholder="Start typing"
             className={styles.searchInpt}
@@ -32,6 +97,16 @@ export const Search = () => {
           <button type="submit" className={styles.findBtn}>
             <IconSearch />
           </button>
+          {searchQuery && isVisible && (
+            <SuggestList
+              data={data}
+              value={value}
+              isFetching={isFetching}
+              isSucces={isSuccess}
+              isLoading={isLoading}
+              isError={isError}
+            />
+          )}
         </form>
       ) : null}
     </>
--- src/shared/hooks/useIsInFavorite.ts
+++ src/shared/hooks/useIsInFavorite.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../api/redux/store";
 import {
   removeFavorite,
@@ -17,36 +17,31 @@ import {
 import useModal from "./useModal";
 
 const useIsInFavorite = (cardId: number) => {
-  const [is, setIs] = useState(false);
+  // const [is, setIs] = useState(false);
   const uid = useAppSelector(selectUid);
   const data = useAppSelector(selectFavorite);
   const isLoading = useAppSelector(selectIsFavoriteLoading);
   const isError = useAppSelector(selectIsFavoriteError);
   const { isOpen, onToggleModal } = useModal();
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    if (data && data.find((el) => el === cardId.toString())) {
-      setIs(true);
-    } else {
-      setIs(false);
-    }
-  }, [data, setIs, cardId]);
+  const is = useMemo(() => {
+    if (data && data.find((el) => el === cardId.toString())) return true;
+    else return false;
+  }, [data, cardId]);
 
   const onTogleFavorite = useCallback(() => {
     if (uid) {
       if (is) {
-        setIs(false);
         dispatch(removeFavorite({ uid: uid, cardId: cardId }));
         onNotifyFavoriteRemoved();
       } else {
-        setIs(true);
         dispatch(setFavorite({ uid: uid, cardId: cardId }));
         onNotifyFavoriteAdded();
       }
     } else {
       onToggleModal();
     }
-  }, [uid, setIs, dispatch, cardId, is, onToggleModal]);
+  }, [uid, dispatch, cardId, is, onToggleModal]);
 
   return {
     favorite: {
